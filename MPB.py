6b395360f04cd48923a7b36eb0134df4c4b6f8e2
LokiLuthor <lokiluthor000@gmail.com>
2024-07-07 22:13:02 +0800

    Update MPB.py

--- MPB.py
+++ MPB.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import plotly.graph_objects as go
 
 st.set_page_config(page_title="Mandatory Pension Booster", page_icon=":bar_chart:",layout="wide")
 
@@ -10,28 +11,29 @@ df = pd.read_excel(
     engine= 'openpyxl',
     sheet_name= 'MPB_DataSet',
     skiprows= 1,
-    usecols= 'A:I',
+    usecols= 'A:J',
     nrows= 18602)
+
+
     
 
-st.dataframe(df)
 
 #--------SIDEBAR------------#
-
+st.logo("Mandatory Pension Booster Calculator.png")
 st.sidebar.header("Please select Starting Age, 2023 MSC, and 2025 MSC")
 
 starting_age = st.sidebar.selectbox(
-    "Select Starting Age:",
+    "Starting Age:",
     options=df["Age"].unique()
 )
 
 MSC_2023 = st.sidebar.selectbox(
-    "Select MSC in 2023:",
+    "MSC in 2023:",
     options=df["MSC1"].unique()
 )
 
 MSC_2025 = st.sidebar.selectbox(
-    "Select MSC in 2025:",
+    "MSC in 2025:",
     options=df["MSC2"].unique()
 )
 
@@ -43,40 +45,28 @@ df_selection = df.query(
 
 
 #---------MAINPAGE---------#
-st.title(":bar_chart: Mandatory Pension Dashboard")
+#st.image("ssspension.png", width=800)
+st.title(":bar_chart: Mandatory Pension Booster Dashboard")
 st.markdown("##")
 
 #TOP KPI's
-start_age = int(df_selection["Age"])
 total_months = int(df_selection["No. Months until Retirement"])
-Contri_2023 = int(df_selection["Contri 1"])
-Contri_2025 = int(df_selection["Contri 2"])
-potential_income = int(df_selection["Potential Income"])
-total_fee = int(df_selection["Total Management fee"])
-TAAV =  int(df_selection["TAAV"])
+Total_Contri = f"₱ {round(float(df_selection['Total Contri']),2):,.2f}"
+Total_Contri1= round(float(df_selection['Total Contri']),2)
+potential_income = f"₱ {round(float(df_selection['Potential Income']),2):,.2f}"
+potential_income1= round(float(df_selection['Potential Income']),2)
+total_fee = f"₱ {round(float(df_selection['Total Management fee']),2):,.2f}"
+total_fee1= round(float(df_selection['Total Management fee']),2)
+AAV =  f"₱ {round(float(df_selection['TAAV']),2):,.2f}"
+AAV1 = round(float(df_selection['TAAV']),2)
 
-column1, column2, column3, column4, column5, column6, column7= st.columns(7)
-with column1:
-    st.subheader("Starting Age")
-    st.subheader(f"{start_age}")
-with column2:
-    st.subheader("Number of Months until Retirement")
-    st.subheader(f"{total_months}")
-with column3:
-    st.subheader("Contribution from 2023 to 2024")
-    st.subheader(f"₱ {Contri_2023:,.2f}")
-with column4:
-    st.subheader("Contribution from 2025")
-    st.subheader(f"₱ {Contri_2025:,.2f}")
-with column5:
-    st.subheader("Potential Income")
-    st.subheader(f"₱ {potential_income:,.2f}")
-with column6:
-    st.subheader("Total Management Fee")
-    st.subheader(f"₱ {total_fee:,.2f}")
-with column7:
-    st.subheader("Total Accumulated Account Value at Retirement")
-    st.subheader(f"₱ {TAAV:,.2f}")
+col1, col2, col3, col4, col5= st.columns(5)
+col1.metric("No. of Months Until Retirement", value = total_months, delta= None, delta_color="normal", help=None, label_visibility="visible")
+col2.metric("Total Contribution", value = Total_Contri, delta= None, delta_color="normal", help=None, label_visibility="visible")
+col3.metric("Potential Income", value = potential_income, delta= None, delta_color="normal", help=None, label_visibility="visible")
+col4.metric("Total Management Fee", value = total_fee, delta= None, delta_color="normal", help=None, label_visibility="visible")
+col5.metric("Total Accumulated Account Value at Retirement", value = AAV, delta= None, delta_color="normal", help=None, label_visibility="visible")
+
 st.markdown("----")
 
 SA = starting_age
@@ -106,7 +96,6 @@ for p in range(1, max + 1):  # Loop from 1 to Max (inclusive)
         WISP_Contribution.append(excess_MSC * PC)
 
 
-# Loop 4: Calculate Income, Account Value, and Fees
 
 ROI=pow(1.06, 1/12) - 1 #return of investment
 MF= pow(1.01, 1/12) - 1 #management fee
@@ -138,26 +127,66 @@ FiveYear_Ages = []
 for age in range(SA, RA + 1, 1):  # Loop in steps of 5 years (inclusive)
     FiveYear_Ages.append(age)
 
+    
 fig_TAAV = px.line(
     x = FiveYear_Ages,
     y = FiveYear_AVMF,
     title ="<b>Growth of Total Accumulated Account Value</b>",
-    template = "plotly_white"
-
+    markers=True
 )
+
 fig_TAAV.update_layout(
+    
     xaxis=dict(
-        title="Ages",  # Set x-axis title
+        title="<b>Age</b>",  # Set x-axis title
         showgrid=True,  # Enable gridlines on the x-axis
         tickmode="linear"
     ),
-    plot_bgcolor="rgba(0,0,0,0)",
+    title_x=0.4,
+    titlefont_size=20,
     yaxis=dict(
-        title="Total Accumulated Account Value",
+        title="<b>Total Accumulated Account Value</b>",
         showgrid = True
     )
 )
 st.plotly_chart(fig_TAAV)
+
+labels = ['Total Contribution', 'Management Fee', 'Potential Income']
+values = [Total_Contri1, total_fee1, potential_income1]
+
+
+
+#Monthly Pension
+Option=['60-month Option', '120-Month Option', '180-Month Option']
+y=[AAV1/60, AAV1/120, AAV1/180]
+formatted_y = [f"<b>₱ {value:,.2f}</b>" for value in y]
+# Use textposition='auto' for direct text
+fig_MP = go.Figure(data=[go.Bar(
+            x=Option, 
+            y=y,
+            text=formatted_y,
+            textposition='auto',
+            textfont_size=20
+)])
+fig_MP.update_layout( title="<b>Duration vs. Pension</b>", 
+                    title_x=0.5,
+                    titlefont_size=20 
+)
+#st.plotly_chart(fig_MP)
+
+#TC vs PI vs MF
+fig_TC = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+fig_TC.update_layout(showlegend=True, 
+                     plot_bgcolor="rgba(0,0,0,0)", 
+                     title="<b>Investment Summary", 
+                     title_x=0.5,
+                     titlefont_size=20
+)
+#st.plotly_chart(fig_TC)
+
+left_column, right_column = st.columns(2)
+left_column.plotly_chart(fig_MP, use_container_width=True)
+right_column.plotly_chart(fig_TC, use_container_width=True)
 
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
@@ -168,3 +197,4 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
+
